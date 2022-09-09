8d886acaa162c54c64746cf3406d12e55e686c9c
Toluwanimi <tolu.mastermindz@gmail.com>
2022-09-09 12:13:05 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main-main block
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+int num;
+int num2;
+int num3;
+for (num = 0; num < 10; num++)
+{
+	for (num2 = num + 1; num2 <= 9; num2++)
+	{
+		for(num3 = num2 + 1; num3 < 10; num3++)
+		{
+			putchar(num + '0');
+			putchar(num2 + '0');
+			putchar(num3 + '0');
+			if (num ! = '9' || num2 != '8' || num3 != '7')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+}
+putchar('\n');
+return (0);
+
+}
