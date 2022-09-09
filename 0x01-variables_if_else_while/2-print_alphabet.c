641faf72e94d55d64dacdc976b583153e21128ff
Toluwanimi <tolu.mastermindz@gmail.com>
2022-09-09 06:21:23 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,12 +8,10 @@
 */
 int main(void)
 {
-
-	char alph;
-
-	for (alph = 'a'; alph <= 'z'; alph++)
-		putchar(alph);
-		putchar('\n');
-	return (0);
+char alph;
+for (alph = 'a'; alph <= 'z'; alph++)
+	putchar(alph);
+	putchar('\n');
+return (0);
 
 }
