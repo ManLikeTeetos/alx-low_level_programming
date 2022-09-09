27e9f1cff05e66ef08f81ffa81eee8bf682c1fa9
Toluwanimi <tolu.mastermindz@gmail.com>
2022-09-09 06:14:43 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main-main block
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+
+	char alph;
+
+	for(alph = 'a'; alph <= 'z'; alph++)
+		putchar(alph);
+		putchar('\n');
+
+	return (0);
+
+}
