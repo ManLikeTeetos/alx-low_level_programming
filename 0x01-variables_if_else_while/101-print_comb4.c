536964f17b54c9f7a0b015a36923d4899fe93a4c
Toluwanimi <tolu.mastermindz@gmail.com>
2022-09-09 12:36:41 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,16 +11,16 @@ int main(void)
 int num;
 int num2;
 int num3;
-for (num = 0; num <= 9; num++)
+for (num = '0'; num <= '9'; num++)
 {
-	for (num2 = num + 1; num2 <= 9; num2++)
+	for (num2 = num + 1; num2 <= '9'; num2++)
 	{
-		for(num3 = num2 + 1; num3 <= 9; num3++)
+		for(num3 = num2 + 1; num3 <= '9'; num3++)
 		{
-			putchar(num + '0');
-			putchar(num2 + '0');
-			putchar(num3 + '0');
-			if (num < 9 || num2 < 8 || num3 < 7)
+			putchar(num);
+			putchar(num2);
+			putchar(num3);
+			if (num != '9' || num2 != '8' || num3 != '7')
 			{
 				putchar(',');
 				putchar(' ');
