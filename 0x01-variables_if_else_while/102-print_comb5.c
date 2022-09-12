c409feebc5b909a7becd18ccc7599e359e5a4a8d
Toluwanimi <tolu.mastermindz@gmail.com>
2022-09-12 02:55:03 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main-main block
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+int num;
+int num2;
+for (num = 0; num < 100; num++)
+{
+	for (num2 = 0; num2 <= 100; num2++)
+	{
+		if (num < num2 )
+		{
+			putchar((num / 10) + 48);
+			putchar((num % 10) + 48);
+			putchar(' ');
+			putchar((num2 / 10) + 48);
+			putchar((num2 % 10) +48);
+			if (num != 98 || num2 != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+}
+putchar('\n');
+return (0);
+
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -10,3 +10,5 @@
 9-print_comb.c : Prints all possible combinations of single-digit numbers.
 100-print_comb3.c : Prints all possible different combinations of two digits.Numbers must be separated by , followed by a space
 
+102-print_comb5.c : Write a program that prints all possible combinations of two two-digit numbers.
+
