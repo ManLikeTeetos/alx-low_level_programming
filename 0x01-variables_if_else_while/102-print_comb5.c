46fdf68698c4bee7406d9fc60650ee0c8181279d
Toluwanimi <tolu.mastermindz@gmail.com>
2022-09-12 03:02:24 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,13 +14,13 @@ for (num = 0; num < 100; num++)
 {
 	for (num2 = 0; num2 < 100; num2++)
 	{
-		if (num < num2 )
+		if (num < num2)
 		{
 			putchar((num / 10) + 48);
 			putchar((num % 10) + 48);
 			putchar(' ');
 			putchar((num2 / 10) + 48);
-			putchar((num2 % 10) +48);
+			putchar((num2 % 10) + 48);
 			if (num != 98 || num2 != 99)
 			{
 				putchar(',');
