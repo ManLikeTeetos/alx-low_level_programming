bfb900a290d4dcbee9545354572d1b4f431ce403
Toluwanimi <tolu.mastermindz@gmail.com>
2022-09-13 15:26:33 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,8 +11,24 @@ void times_table(void)
 		for (f = 0; f < 10; f++)
 		{
 			a = f * n;
-			_putchar(a + '0');
-			_putchar(',');
+			if (a == 0)
+			{
+				_putchar(a + '0');
+			}
+
+			if (a < 10 && a != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(a + '0');
+			} else if ( a >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((a / 10) + '0');
+				_putchar((a % 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}
