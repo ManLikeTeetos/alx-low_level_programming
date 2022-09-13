a1206e80c7c8c7d4593510b3d0170d59dc10ef7c
Toluwanimi <tolu.mastermindz@gmail.com>
2022-09-13 12:05:59 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*main - main block
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+	char alph;
+	for (alph = 'a'; alph < 'z'; alph++)
+		_putchar('alph');
+_putchar('\n');
+return 0;
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,3 @@
 0-putchar.c : Prints out "_putchar" followed by a new line
+1-alphabet.c : Print all the alphabets in small letters
+
