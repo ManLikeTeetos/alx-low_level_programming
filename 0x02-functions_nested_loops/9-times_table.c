8c4df9edb1fb2f1397d043bc3a2995549ae77179
Toluwanimi <tolu.mastermindz@gmail.com>
2022-09-13 15:19:06 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,11 +6,14 @@ void times_table(void)
 {
 	int n, f, a;
 
-	n = 9;
-	for (f = 0; f <= 10; f++)
+	for (n = 0; n < 10; n++)
 	{
-		a = f * n;
-		_putchar(a);
-		_putchar(',');
+		for (f = 0; f < 10; f++)
+		{
+			a = f * n;
+			_putchar(a + '0');
+			_putchar(',');
+		}
+		_putchar('\n');
 	}
 }
