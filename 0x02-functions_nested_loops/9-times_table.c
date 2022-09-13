bb31a6ead58852db135a560c797e8e5f4aac88c6
Toluwanimi <tolu.mastermindz@gmail.com>
2022-09-13 15:08:41 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*times_table - Prints the 9 times table
+*/
+void times_table(void)
+{
+	int n, f, a;
+
+	n = 9;
+	for (f = 0; f <= 10; f++)
+	{
+		a = f * 9;
+		_putchar(a);
+		_putchar(',');
+	}
+}
