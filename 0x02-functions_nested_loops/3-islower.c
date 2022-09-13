35d28bb30942ec5b9fa470fb32585b3b39416fec
Toluwanimi <tolu.mastermindz@gmail.com>
2022-09-13 13:10:27 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,13 @@
 #include <ctype.h>
 /**
 *_islower - Checks if an alphabet is lowercase or not
+*@c: the input character
+*Return: returns 1 f lowercase, 0 if uppercase
 */
 int _islower(int c)
 {
 	if (islower(c))
 		return (1);
-	else 
+	else
 		return (0);
 }
