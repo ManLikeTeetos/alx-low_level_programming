53ddf05f4887ccaa773a1f1b30c9d860e4fc5969
Toluwanimi <tolu.mastermindz@gmail.com>
2022-09-13 17:09:45 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+*main - main block
+*Return: Always zero (success)
+*/
+int main(void)
+{
+	unsigned long int l, m, n, m_one, m_two, n_one, n_two;
+
+	m = 1;
+	n = 2;
+	
+	printf("%lu", m);
+
+	for (l = 1; l < 91; l++)
+	{
+		printf(", %lu", n);
+		n = n + m;
+		m = n - m;
+	}
+	m_one = m / 1000000000;
+	m_two = m % 1000000000;
+	n_one = n / 1000000000;
+	n_two = n % 1000000000;
+
+	for (l = 92; l < 99; ++l)
+	{
+			printf(", %lu", n_one + (n_two / 1000000000));
+			printf("%lu", n_two % 1000000000);
+			n_one = n_one + m_one;
+			m_one = n_one - m_one;
+			n_two = n_two + m_two;
+			m_two = n_two - m_two;
+	}
+	printf("\n");
+	return (0);
+}
+
