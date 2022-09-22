77c443d7f748e49afa82bf63d7f90d4d542dea30
ManLikeTeetos <tolu.mastermindz@gmail.com>
2022-09-22 12:33:41 +0000

    C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strcmp - compares two strings
+ *@s1: First string
+ *@s2: Second string to added
+ *Return: returns res, true or false
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int res = strcmp(dest, src);
+return (res);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 0-strcat.c : Concatenates two string <br/>
 1-strncat.c : Concatenates two string with a n as a specifier of characters <br/>
 2-strncpy.c : A function that copies a string <br/>
+3-strcmp.c : Compares two strings <br/>
