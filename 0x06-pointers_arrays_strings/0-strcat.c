68e5a5900f8c212468a1b5bcaf77776eac073807
ManLikeTeetos <tolu.mastermindz@gmail.com>
2022-09-22 11:57:50 +0000

    C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strcat - concatenates two string
+ *@dest: string to append to
+ *@src: string to added
+ *Return: returns dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+strcat(dest, src);
+return (dest);
+}
