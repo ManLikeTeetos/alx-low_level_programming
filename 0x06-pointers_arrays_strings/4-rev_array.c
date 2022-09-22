11f9fc7ef26f85403d83ea6c15e643b9cdd9e197
ManLikeTeetos <tolu.mastermindz@gmail.com>
2022-09-22 13:27:18 +0000

    C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *reverse_array - reverses integer in an array
+ *@a: pointer of integer array
+ *@n: number of element in the array
+ */
+void reverse_array(int *a, int n)
+{
+int l, m, p;
+l = n - 1;
+for (m = 0; m < n; m++)
+{
+p = a[m];
+a[m] = a[l];
+a[l--] = p;
+}
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -2,3 +2,4 @@
 1-strncat.c : Concatenates two string with a n as a specifier of characters <br/>
 2-strncpy.c : A function that copies a string <br/>
 3-strcmp.c : Compares two strings <br/>
+4-rev_array.c : a function that reverses the content of an array of integers. <br/>
