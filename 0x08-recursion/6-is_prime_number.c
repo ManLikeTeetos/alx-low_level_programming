06486961730c794cad4ee91e7a985a5d999edd1e
ManLikeTeetos <tolu.mastermindz@gmail.com>
2022-09-28 17:32:46 +0000

    C - Recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+int first_prime(int n, int i);
+
+/**
+ * is_prime_number - says if an integer is a prime number or not
+ * @n: number to check
+ * Return: 1 if n is a prime number, 0 if it is not
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (first_prime(n, n - 1));
+}
+
+/**
+ * first_prime - calculates if a number is prime
+ * @n: number
+ * @i: iterator
+ *
+ * Return: 1 if n is prime, 0 if it is not
+ */
+int first_prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (actual_prime(n, i - 1));
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -4,3 +4,4 @@
 [3-factorial.c](./3-factorial.c) : a function that returns the factorial of a given number. <br/>
 [4-pow_recursion.c](./4-pow_recursion.c) : a function that returns the value of x raised to the power of y. <br/>
 [5-sqrt_recursion.c](./5-sqrt_recursion.c) : a function that returns the natural square root of a number. <br/>
+[6-is_prime_number.c](./6-is_prime_number.c) : a function that returns 1 if the input integer is a prime number, otherwise return 0. <br/>
